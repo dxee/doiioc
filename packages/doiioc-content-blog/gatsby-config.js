cee1f263454413cf0df93a47614d2074c268f3e1
lovoop <bing.fxx@gmail.com>
2019-05-16 19:16:59 +0800

    chore: Remove images folder as source

--- packages/doiioc-content-blog/gatsby-config.js
+++ packages/doiioc-content-blog/gatsby-config.js
@@ -2,7 +2,6 @@ module.exports = config => {
   const plugins = []
 
   return {
-    __experimentalThemes: [],
     plugins: [
       ...plugins,
       {
--- packages/doiioc-theme-default/gatsby-config.js
+++ packages/doiioc-theme-default/gatsby-config.js
@@ -4,18 +4,10 @@ module.exports = options => {
   const { mdxLayouts = {} } = options
 
   return {
-    __experimentalThemes: [],
     plugins: [
       'gatsby-mdx',
       'gatsby-plugin-react-helmet',
       'gatsby-plugin-styled-components',
-      {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-          name: `images`,
-          path: `${__dirname}/src/images`
-        }
-      },
       {
         resolve: 'gatsby-transformer-remark',
         options: {
